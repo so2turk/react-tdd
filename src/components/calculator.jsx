a56e5ff759684da9c99752508c8cd6e7a247ded7
Serhat Ozturk <so2turk@yahoo.com>
2022-03-24 13:00:12 +0100

    Fulfill setup tests

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -3,6 +3,13 @@ const Calculator = () => {
 
   return (
     <>
+      <h1 data-testid='title'>Calculator</h1>
+      <h1 data-testid='result'>0</h1>
+      <input data-testid='input' value={1} />
+      <button data-testid='add-btn'>+</button>
+      <button data-testid='subt-btn'>-</button>
+      <button data-testid='mult-btn'>*</button>
+      <button data-testid='div-btn'>÷</button>
     </>
   )
 }

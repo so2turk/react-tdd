088aad6e96ca0ee6a2623fb7f27fa622178dbe01
Serhat Ozturk <so2turk@yahoo.com>
2022-03-29 12:08:10 +0200

    Describe tests for calculator functionality

--- src/components/__test__/calculator.test.js
+++ src/components/__test__/calculator.test.js
@@ -1,5 +1,5 @@
 import Calculator from '../calculator'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
 describe('tests for calculator setup', () => {
   
@@ -50,6 +50,83 @@ describe('tests for calculator setup', () => {
     const divBtn = screen.getByTestId('div-btn')
 
     expect(divBtn.textContent).toBe('÷')
+  })
+
+})
+
+describe('tests for calculator fucntionality', () => {
+
+  test('change input value', () => {
+    render(<Calculator />)
+    const inputEl = screen.getByTestId('input')
+
+    fireEvent.change(inputEl, {
+      target: {
+        value: "2"
+      }
+    })
+
+    expect(inputEl.value).toBe('2')
+  })
+
+  test('click on addBtn adds 1 to result', () => {
+    render(<Calculator />)
+    const addBtn = screen.getByTestId('add-btn')
+    const resultEl = screen.getByTestId('result')
+
+    expect(resultEl.textContent).toBe('0')
+    fireEvent.click(addBtn)
+    expect(resultEl.textContent).toBe('1')
+  })
+
+  test('click on subtBtn subtracts 1 from result', () => {
+    render(<Calculator />)
+    const subtBtn = screen.getByTestId('sbut-btn')
+    const resultEl = screen.getByTestId('result')
+
+    expect(resultEl.textContent).toBe('0')
+    fireEvent.click(subtBtn)
+    expect(resultEl.textContent).toBe('-1')
+  })
+
+  test('click on multBtn multiplies result', () => {
+    render(<Calculator />)
+    const inputEl = screen.getByTestId('input')
+    const resultEl = screen.getByTestId('result')
+    const addBtn = screen.getByTestId('add-btn')
+    const multBtn = screen.getByTestId('mult-btn')
+
+    fireEvent.change(inputEl, {
+      target: {
+        value: "2"
+      }
+    })
+    expect(inputEl.value).toBe('2')
+    expect(resultEl.textContent).toBe('0')
+    fireEvent.click(addBtn)
+    expect(resultEl.textContent).toBe('2')
+    fireEvent.click(multBtn)
+    expect(resultEl.textContent).toBe('4')
+  })
+
+  test('click on divBtn divides result', () => {
+    render(<Calculator />)
+    const inputEl = screen.getByTestId('input')
+    const resultEl = screen.getByTestId('result')
+    const addBtn = screen.getByTestId('add-btn')
+    const divBtn = screen.getByTestId('div-btn')
+
+    fireEvent.change(inputEl, {
+      target: {
+        value: "2"
+      }
+    })
+    expect(inputEl.value).toBe('2')
+    expect(resultEl.textContent).toBe('0')
+    fireEvent.click(addBtn)
+    expect(resultEl.textContent).toBe('2')
+    fireEvent.click(divBtn)
+    expect(resultEl.textContent).toBe('1')
   })
 
 })
